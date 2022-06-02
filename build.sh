369168ff04ac1a7d3b6a074e1682b946ce229a51
pepperoni505 <pepperoni505@gmail.com>
2022-06-01 21:50:47 -0400

    fix: incorrect build process

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 docker run \
     --rm \
     -v `pwd`:/jebediah \
+    -v "$HOME/.dockercache/gradle":"/root/.gradle" \
     mingc/android-build-box bash \
-    -c 'cd jebediah;./gradlew build'
+    -c 'update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java;cd /jebediah;./gradlew assembleDebug'
