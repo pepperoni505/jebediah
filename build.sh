a288ed36b97462432ef64211c35593697716540a
pepperoni505 <49950581+pepperoni505@users.noreply.github.com>
2022-06-04 15:44:36 -0400

    fix: build.sh issues

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 docker run \
     --rm \
-    -v `pwd`:/jebediah \
+    -v "$(pwd):/jebediah" \
     -v "$HOME/.dockercache/gradle":"/root/.gradle" \
     mingc/android-build-box bash \
     -c 'update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java;cd /jebediah;./gradlew assembleDebug'
