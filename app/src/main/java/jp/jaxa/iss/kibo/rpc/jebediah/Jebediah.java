394647934cb1f51a19d385f89d7b745c990f9038
pepperoni505 <pepperoni505@gmail.com>
2022-06-04 22:18:24 -0400

    feat: jebediah class

---
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/jebediah/Jebediah.java
@@ -0,0 +1,14 @@
+package jp.jaxa.iss.kibo.rpc.jebediah;
+
+enum JebediahState {
+    START,
+    END,
+}
+
+public class Jebediah {
+
+    public void run() {
+        System.out.println("moving the ");
+    }
+
+}
--- app/src/main/java/jp/jaxa/iss/kibo/rpc/jebediah/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/jebediah/YourService.java
@@ -3,19 +3,19 @@ package jp.jaxa.iss.kibo.rpc.jebediah;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 import jp.jaxa.iss.kibo.rpc.jebediah.KiboConstants.*;
 
+import org.opencv.core.Mat;
+
 /**
  * Class meant to handle commands from the Ground Data System and execute them in Astrobee
  */
 
 public class YourService extends KiboRpcService {
+
+    Jebediah jeb = new Jebediah();
+
     @Override
     protected void runPlan1() {
-        // write here your plan 1
-        api.startMission();
-        api.moveTo(KiboConstants.POINT_1_POS, KiboConstants.POINT_1_ROT, true);
-
-        // send mission completion
-        api.reportMissionCompletion();
+        jeb.run();
     }
 
     @Override
